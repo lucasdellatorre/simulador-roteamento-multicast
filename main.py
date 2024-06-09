1ea98aa70e55b3d5d7caa73221750023fbbc7dd8
lucasdellatorre <lucasdellatorre222@gmail.com>
2024-06-09 17:41:56 -0300

    read args from cli

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from file_reader import FileReader
 from exec_manager import ExecManager
+import sys
 
 def main():
-    filename = "topologia1.txt"
+    if len(sys.argv) < 3:
+        print("Usage: python main.py <topologia.txt> <exec.txt>")
+        sys.exit(1)
+    
+    filename = sys.argv[1]
     backbone = FileReader(filename).get_backbone()
-    exec_file_name = "exec.txt"
+    exec_file_name = sys.argv[2]
     exec_instructions = FileReader(exec_file_name).parse_execution_from_exec_file()
     
     exec_manager = ExecManager(exec_instructions, backbone)
     exec_manager.run()
-    # print(f'\n{backbone}')
+    # print(f'\n{backbone}')    
 if __name__ == '__main__':
     main()
